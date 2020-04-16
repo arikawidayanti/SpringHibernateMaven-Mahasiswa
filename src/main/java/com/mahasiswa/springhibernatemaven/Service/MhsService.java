49d67ca6870600a13d15799bdf6df5ba8566fef4
arikawidayanti <63407799+arikawidayanti@users.noreply.github.com>
2020-04-16 21:01:58 +0700

    Create MhsService.java

---
+++ src/main/java/com/mahasiswa/springhibernatemaven/Service/MhsService.java
@@ -0,0 +1,16 @@
+package com.crud.mahasiswa.MavenProject.service;
+
+import com.crud.mahasiswa.MavenProject.model.Mahasiswa;
+import java.util.List;
+
+/**
+ *
+ * @author HP Undang Purnama
+ */
+public interface MahasiswaService {
+    public List<Mahasiswa> getMhs();
+    public Mahasiswa getMahasiswa(Integer id);
+    public Mahasiswa saveMahasiswa(Mahasiswa mahasiswa);
+    public Mahasiswa updateMahasiswa(Mahasiswa mahasiswa);
+    public Mahasiswa deleteMahasiswa(Mahasiswa mahasiswa);
+}
